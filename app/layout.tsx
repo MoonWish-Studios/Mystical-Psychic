7e8d6b8292c71961c0f658551d0c7523df4c3321
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-30 10:58:24 -0800

    added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import "./styles/globals.css"
 import Image from "next/image"
+import { Outfit } from "@next/font/google"
+
+const outfit = Outfit({ subsets: ["latin"] })
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={outfit.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
 
-      <body className="h-[200vh]">
-        <div className="fixed h-screen m-0 w-screen -z-50">
+      <body className="">
+        <div className="fixed h-screen m-0 p-0 w-screen -z-50">
           <Image
-            className=" object-cover"
+            className="object-cover"
             src="/nebula.gif"
             alt="nebula background gif"
             fill
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import BookBox from "./components/BookBox"
 // download headwind extension to autosort classnames so we see the same thing
 export default function Home() {
   return (
-    <div className="w-screen flex items-center justify-center">
+    <div className="w-full px-12 flex items-center justify-center">
       <BookBox imgName="buddha.png" />
     </div>
   )
